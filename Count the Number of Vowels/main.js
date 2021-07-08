793c2c60962e9c3ab5014820c4652ff6a71ffd9e
rasyidzkun <rasyidzkun@gmail.com>
2021-07-08 12:14:02 +0700

    Menyebutkan huruf yang termasuk vokal

--- Count the Number of Vowels/main.js
+++ Count the Number of Vowels/main.js
@@ -1,9 +1,17 @@
 const str = prompt("Masukkan sebuah string");
 
-const countVowel = () => {
+const countVowelLength = () => {
   const count = str.match(/[aiueo]/gi).length;
   return count;
 };
 
-const resultVowel = countVowel();
-alert(`Jumlah huruf vokal adalah ${resultVowel}`);
+const countVowel = () => {
+  const count = str.match(/[aiueo]/gi);
+  return count;
+};
+
+const resultVowelLength = countVowelLength();
+const resultCountVowel = countVowel();
+alert(
+  `Jumlah huruf vokal adalah ${resultVowelLength}, yaitu ${resultCountVowel}`
+);
