4d94934b2f35ecf2b1d3dc74d355aeb7f999f971
rasyidzkun <rasyidzkun@gmail.com>
2021-07-08 11:59:00 +0700

    menambahkan program penghitung huruf konsonan

---
+++ Count the Number of Consonants/main.js
@@ -0,0 +1,17 @@
+const str = prompt("Masukkan sebuah string");
+
+const countConsonantLength = () => {
+  const count = str.match(/[^aiueo]/gi).length;
+  return count;
+};
+
+const countConsonant = () => {
+  const count = str.match(/[^aiueo ]/gi);
+  return count;
+};
+
+const resultConsonantLength = countConsonantLength();
+const resultConsonant = countConsonant();
+alert(
+  `Jumlah huruf konsonan adalah ${resultConsonantLength}, yaitu ${resultConsonant}`
+);
