0a1f54bb009096952b51716d27b39cd8711eac59
rasyidzkun <rasyidzkun@gmail.com>
2021-07-14 09:49:09 +0700

    add number pattern program

---
+++ Number Pattern/main.js
@@ -0,0 +1,15 @@
+for (let i = 0; i <= 10; i++) {
+  for (let j = i; j <= 10; j++) {
+    document.write(`${j} `);
+  }
+  document.write("<br>");
+}
+
+document.write("<br>");
+
+for (let i = 0; i <= 10; i++) {
+  for (let j = i; j >= 0; j--) {
+    document.write(`${j} `);
+  }
+  document.write("<br>");
+}
