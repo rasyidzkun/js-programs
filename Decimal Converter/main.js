e8c4371541b1de380f85c8ba1be30ad823a5138c
rasyidzkun <rasyidzkun@gmail.com>
2021-07-14 10:57:13 +0700

    change hexadecimal uppercase

--- Decimal Converter/main.js
+++ Decimal Converter/main.js
@@ -1,20 +1,20 @@
 function binary() {
-  input = Number(prompt(" Masukkan angka decimal ", 0));
+  let input = Number(prompt(" Masukkan angka decimal ", 0));
 
   let biner = input.toString(2);
   alert(`decimal = ${input} biner = ${biner}`);
 }
 
 function octal() {
-  input = Number(prompt(" Masukkan angka decimal ", 0));
+  let input = Number(prompt(" Masukkan angka decimal ", 0));
 
   let octal = input.toString(8);
   alert(`decimal = ${input} oktal = ${octal}`);
 }
 
 function hexadecimal() {
-  input = Number(prompt(" Masukkan angka decimal ", 0));
+  let input = Number(prompt(" Masukkan angka decimal ", 0));
 
-  let hex = input.toString(16);
+  let hex = input.toString(16).toUpperCase();
   alert(`decimal = ${input} hexadecimal = ${hex}`);
 }
