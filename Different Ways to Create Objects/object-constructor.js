68f30733111466c4b545cab8c7d7cba3cf99f21f
rasyidzkun <rasyidzkun@gmail.com>
2021-07-18 13:09:45 +0700

    simple refactoring

--- Different Ways to Create Objects/object-constructor.js
+++ Different Ways to Create Objects/object-constructor.js
@@ -1,15 +1,9 @@
 function User(name, email) {
   this.name = name;
   this.email = email;
-  this.login = function () {
-    console.log(`${this.email} just logged in`);
-  };
-  this.getName = function () {
-    console.log(`Your username is ${this.name}`);
-  };
-  this.logout = function () {
-    console.log(`${this.email} just logged out`);
-  };
+  this.login = () => console.log(`${this.email} just logged in`);
+  this.getName = () => console.log(`Your username is ${this.name}`);
+  this.logout = () => console.log(`${this.email} just logged out`);
 }
 
 let userOne = new User("Ar Rasyid", "rasyidzkun.gmail.com");
