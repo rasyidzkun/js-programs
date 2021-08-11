7a773f16463b68fc7cac8cdcb711a2331dcc926b
rasyidzkun <rasyidzkun@gmail.com>
2021-08-11 10:16:29 +0700

    add program to check missing letters

---
+++ Missing Letter/main.js
@@ -0,0 +1,24 @@
+// Find the missing letter in the passed letter range and return it. If all letters are present, return undefined
+// ex.
+// missingLetters("abce") == "d"
+// missingLetters("abcdefghjklmno") == "i"
+// missingLetters("abcdefghijklmnopqrstuvwxyz") == undefined
+
+function missingLetters(str) {
+  let compare = str.charCodeAt(0);
+  let missing;
+
+  str.split("").map((char, i) => {
+    if (str.charCodeAt(i) == compare) {
+      ++compare;
+    } else {
+      missing = String.fromCharCode(compare);
+    }
+  });
+
+  return missing;
+}
+
+console.log(missingLetters("abce"));
+console.log(missingLetters("abcdefghjklmno"));
+console.log(missingLetters("abcdefghijklmnopqrstuvwxyz"));
