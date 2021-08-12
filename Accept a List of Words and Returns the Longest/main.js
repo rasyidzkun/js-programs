62069921e702e9aa5372f947a8c343cfa2d61e11
rasyidzkun <rasyidzkun@gmail.com>
2021-08-12 10:16:05 +0700

    return a longest word

---
+++ Accept a List of Words and Returns the Longest/main.js
@@ -0,0 +1,12 @@
+// Write a JavaScript function that accept a list of country names as input and returns the longest country name as output
+
+// Sample function : Longest_Country_Name(["Australia", "Germany", "United States of America"])
+// Expected output : "United States of America"
+
+function longest(arr) {
+  return arr.reduce((long, short) => {
+    return long.length > short.length ? long : short;
+  });
+}
+
+console.log(longest(["Australia", "Germany", "United States of America"]));
