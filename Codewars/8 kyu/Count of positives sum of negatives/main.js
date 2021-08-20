340b89aa712a9a896c5e975442eba9608a08d537
Ar Rasyid Sarifullah <rasyidzkun@gmail.com>
2021-08-20 11:12:09 +0700

    Delete Codewars/8 kyu/Count of positives sum of negatives directory

--- Codewars/8 kyu/Count of positives sum of negatives/main.js
+++
@@ -1,9 +0,0 @@
-function countPositivesSumNegatives(input) {
-  if (input && input.length) {
-    let positive = 0;
-    let negative = 0;
-    input.forEach((num) => (num > 0 ? positive++ : (negative += num)));
-    return [positive, negative];
-  }
-  return [];
-}
